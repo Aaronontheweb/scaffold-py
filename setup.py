2fb6ef6d0814e74e2fed292e75ca7ddbbeb218ea
Aaron Stannard <aaron@aaronstannard.com>
2011-12-04 11:18:56 -0800

    Added download details to setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ except ImportError:
 config = {
     'description': 'Simple project scaffolding for Python',
     'author': 'Aaron Stannard',
-    'url': 'URL to get it at.',
-    'download_url': 'Where to download it.',
+    'url': 'https://github.com/Aaronontheweb/scaffold-py',
+    'download_url': 'https://github.com/Aaronontheweb/scaffold-py/zipball/master',
     'author_email': 'aaron@stannardlabs.com',
     'version': '0.1',
     'install_requires': ['nose'],
