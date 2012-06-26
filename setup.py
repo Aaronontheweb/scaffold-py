0b40623987133f2c0fc67764d1917bc0c4a09d79
Aaronontheweb <aaron@aaronstannard.com>
2012-06-25 19:16:10 -0700

    updated the download url in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ config = {
     'description': 'Simple project scaffolding for Python',
     'author': 'Aaron Stannard',
     'url': 'https://github.com/Aaronontheweb/scaffold-py',
-    'download_url': 'https://github.com/Aaronontheweb/scaffold-py/zipball/master',
+    'download_url': 'https://github.com/downloads/Aaronontheweb/scaffold-py/Scaffold-0.1.1.tar.gz',
     'author_email': 'aaron@stannardlabs.com',
     'version': '0.1.1',
     'install_requires': ['nose'],
