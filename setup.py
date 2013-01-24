4d5f955254757a02523c927bc97179914c1e88e3
Aaron Stannard <aaron@aaronstannard.com>
2013-01-24 14:22:29 -0800

    fixed the setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ config = {
     'description': 'Simple project scaffolding for Python',
     'author': 'Aaron Stannard',
     'url': 'https://github.com/Aaronontheweb/scaffold-py',
-    'download_url': 'https://github.com/downloads/Aaronontheweb/scaffold-py/Scaffold-0.1.3.tar.gz',
+    'download_url': 'https://github.com/Aaronontheweb/scaffold-py/archive/v0.1.3.tar.gz',
     'author_email': 'aaron@stannardlabs.com',
     'version': '0.1.3',
     'install_requires': ['nose'],
