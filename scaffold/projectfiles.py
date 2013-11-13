4dd325f05557e5e78733a126149ebd72ccd88e7a
Zearin <zearin@gonk.net>
2013-11-13 16:06:47 -0500

    projectfiles.py: Minor docstring tweaks

--- scaffold/projectfiles.py
+++ scaffold/projectfiles.py
@@ -1,10 +1,10 @@
-"""An extremely ghetto, but functional way of creating the necessary .py files for the project setup"""
+"""An extremely ghetto (but functional) way to create the necessary .py files for the scaffold"""
 import os
 import projectfolders
 import subprocess
 
 def create_files(project_name, root_dir):
-    """Creates all of the necessary files for a given project skeleton"""
+    """Creates all files necessary for a project skeleton"""
     root_dir = projectfolders.create_path(root_dir, project_name) #Modify the root
     
     write_setup(project_name, root_dir)
@@ -12,7 +12,7 @@ def create_files(project_name, root_dir):
     write_tests(project_name, root_dir)
 
 def write_setup(project_name, root_dir):
-    """Writes the default setup.py file"""
+    """Writes a default setup.py file"""
     setup_path = get_file_path(root_dir, None, "setup.py") #Get the path for setup.py
     setup_content = get_setup_text(project_name)
     
@@ -22,8 +22,8 @@ def write_setup(project_name, root_dir):
     print_file(setup_path, " +++")
     
 def write_tests(project_name, root_dir):
-    """Writes our tests/NAME_tests.py file to disk"""
     test_path = get_file_path(root_dir, "tests", "{project_name}_tests.py".format(project_name=project_name)) #Get the path for setup.py
+    """Writes tests/PROJECT_NAME_tests.py file to disk"""
     test_content = get_test_text(project_name)
     
     test_file = open(test_path, 'w')
@@ -32,7 +32,7 @@ def write_tests(project_name, root_dir):
     print_file(test_path)
 
 def write_inits(project_name, root_dir):
-    """Creates all of the __init__.py files necessary for the project skeleton"""
+    """Creates all __init__.py files necessary for the project skeleton"""
     
     #Create our file paths first...
     test_init_path = get_file_path(root_dir, "tests", "__init__.py")
@@ -89,9 +89,9 @@ setup(**config)
     return setup_text
 
 def get_test_text(project_name):
-    """Again, quite ghetto and can probably be improved, but it works"""
     
     return """
+    #Again, quite ghetto and can probably be improved, but it works
 from nose.tools import *
 import {PROJECT}
 
